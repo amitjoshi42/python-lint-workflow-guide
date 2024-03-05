0ad799c8c977026b4aef60acfcd4cf689dba968b
Shubham Jha <45190857+j-shubham@users.noreply.github.com>
2024-03-05 16:34:28 +0530

    Update app.py
    Adding one more function for multiplication and added 2 black line space in between as sample.

--- app.py
+++ app.py
@@ -6,7 +6,13 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+
+def mult(a, b):
+    return a * b
+
+
 # Sample usage
 if __name__ == "__main__":
     print(add(5, 3))
     print(subtract(5, 3))
+    print(mult(5, 3))
